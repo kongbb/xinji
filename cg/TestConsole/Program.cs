ba48ddf733658610de83629395720ef0a1f23fe3
roger <roger8297@hotmail.com>
2013-07-17 16:09:38 +0900

    add database & test project, add Thinktecture dll

---
+++ cg/TestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            client.SetBasicAuthentication();
+        }
+    }
+}
