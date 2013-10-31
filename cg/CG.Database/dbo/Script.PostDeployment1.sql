00e09208c143f5b3c160bd192736d5f067b1efc5
neo <canguan168@gmail.com>
2013-10-31 12:14:15 +0800

    added new data samples
    包括2次点菜
    order status, order type,
    ordertypetax,order,tablemeal,tablemealorder,orderitem

--- cg/CG.Database/dbo/Script.PostDeployment1.sql
+++ cg/CG.Database/dbo/Script.PostDeployment1.sql
@@ -37,6 +37,10 @@ INSERT INTO [dbo].[User] values (2, 'waiter', 'BALIE', 'Laurent', 'password', 1,
 INSERT INTO [dbo].[MenuItemCategory] values (1,'Wine',1)
 INSERT INTO [dbo].[MenuItemCategory] values (2,'Chicken',1)
 INSERT INTO [dbo].[MenuItemCategory] values (3,'Beef',1)
+INSERT INTO [dbo].[MenuItemCategory] values (4,'Fish',1)
+INSERT INTO [dbo].[MenuItemCategory] values (5,'Pork',1)
+INSERT INTO [dbo].[MenuItemCategory] values (6,'Vegetables',1)
+INSERT INTO [dbo].[MenuItemCategory] values (7,'Pizza',1)
 
 
 -- Menu Item
@@ -68,6 +72,26 @@ INSERT INTO [dbo].[MenuItem] values (23,'Beef soute',5,1,'2013-10-21 15:34',1,3)
 INSERT INTO [dbo].[MenuItem] values (24,'Beef rice',9,1,'2013-10-21 15:34',1,3)
 INSERT INTO [dbo].[MenuItem] values (25,'Beef brioche',3,1,'2013-10-21 15:34',1,3)
 
+INSERT INTO [dbo].[MenuItem] values (26,'Big fish',19.9,1,'2013-10-30 15:33',1,4)
+INSERT INTO [dbo].[MenuItem] values (27,'Fried fish',28,1,'2013-10-30 15:33',1,4)
+INSERT INTO [dbo].[MenuItem] values (28,'Saumon Sandwich',15,1,'2013-10-30 15:33',1,4)
+INSERT INTO [dbo].[MenuItem] values (29,'Tuna Salad',20,1,'2013-10-30 15:33',1,4)
+INSERT INTO [dbo].[MenuItem] values (30,'Saumon Pasta',20,1,'2013-10-30 15:33',1,4)
+
+INSERT INTO [dbo].[MenuItem] values (31,'Pork roste',10.90,1,'2013-10-30 15:33',1,5)
+INSERT INTO [dbo].[MenuItem] values (32,'Sault Pork',12,1,'2013-10-30 15:33',1,5)
+INSERT INTO [dbo].[MenuItem] values (33,'roti',19.9,1,'2013-10-30 15:33',1,5)
+
+INSERT INTO [dbo].[MenuItem] values (34,'Tofu Vegetable',10.90,1,'2013-10-30 15:25',1,6)
+INSERT INTO [dbo].[MenuItem] values (35,'Salad Napoli',12,1,'2013-10-30 15:40',1,6)
+INSERT INTO [dbo].[MenuItem] values (36,'Salad Maroc',25,1,'2013-10-30 15:43',1,6)
+
+INSERT INTO [dbo].[MenuItem] values (37,'Pizza Napoli',11.00,1,'2013-10-30 15:20',1,7)
+INSERT INTO [dbo].[MenuItem] values (38,'Pizza Four Season',12.0,1,'2013-10-20 15:40',1,7)
+INSERT INTO [dbo].[MenuItem] values (39,'Pizza Toulon',13.9,1,'2013-10-30 15:20',1,7)
+INSERT INTO [dbo].[MenuItem] values (40,'Pizza 4 formage',14.9,1,'2013-10-30 15:20',1,7)
+
+
 
 -- Shift Type
 INSERT INTO [dbo].[ShiftType] values (1,'Midi',1,'2013-10-21 11:00','2013-10-21 14:00')
@@ -92,10 +116,6 @@ INSERT INTO [dbo].[MenuItemShift] values (9,4,1, 15)
 INSERT INTO [dbo].[PaymentType] values (1, 'Cash')
 INSERT INTO [dbo].[PaymentType] values (2, 'Credit Card')
 INSERT INTO [dbo].[PaymentType] values (3, 'Gift Ticket')
-
--- Tax
-INSERT INTO [dbo].[Tax] values (1, 'Chinese Services Tax',0.19,1)
-INSERT INTO [dbo].[Tax] values (2, 'Chinese Avt',0.11,1)
 
 -- Terminal
 INSERT INTO [dbo].[Terminal] values (1, 'Cash box',1)
@@ -126,3 +146,63 @@ INSERT INTO [dbo].[TableStatus] values (3,'NeedWaiter')
 INSERT INTO [dbo].[TableStatus] values (4,'Dining')
 INSERT INTO [dbo].[TableStatus] values (5,'Billing')
 INSERT INTO [dbo].[TableStatus] values (6,'NeedClean')
+INSERT INTO [dbo].[TableStatus] values (7,'Paid')
+
+-- Tax
+INSERT INTO [dbo].[Tax] values (1, 'Chinese Services Tax',0.19,1)
+INSERT INTO [dbo].[Tax] values (2, 'Chinese Avt',0.11,1)
+
+-- Order Type
+INSERT INTO [dbo].[OrderType] values (1,'At Reataurant')
+INSERT INTO [dbo].[OrderType] values (2,'Take Away')
+
+-- OrderType Tax
+INSERT INTO [dbo].[OrderTypeTax] values (1,1,1)
+INSERT INTO [dbo].[OrderTypeTax] values (2,2,2)
+
+-- Order Status
+INSERT INTO [dbo].[OrderStatus] values (1,'Openning')
+INSERT INTO [dbo].[OrderStatus] values (2,'Printed to kitchen')
+INSERT INTO [dbo].[OrderStatus] values (3,'Delivered')
+INSERT INTO [dbo].[OrderStatus] values (4,'Void')
+
+-- 下面是点2次点菜的数据  
+-- Table 1   20:00开台 20:15点菜  21:30结账关闭  4人消费 4个菜 数量各1 服务员ID=2 At Restaurant 消费税19%
+INSERT INTO [dbo].[TableMeal] values (1,1,7,'2013-10-31 20:00','2013-10-31 21:30',4)
+INSERT INTO [dbo].[Order] values (1,1,3,1,'2013-10-31 20:15',2,'2013-10-31 20:20',2,1)
+INSERT INTO [dbo].[TableMealOrder] values (1,1,1)
+INSERT INTO [dbo].[OrderItem] values (1,1,1,1)
+INSERT INTO [dbo].[OrderItem] values (2,1,2,1)
+INSERT INTO [dbo].[OrderItem] values (3,1,3,1)
+INSERT INTO [dbo].[OrderItem] values (4,1,4,1) 
+INSERT INTO [dbo].[Transaction] values (1,1,1,'2013-10-31 21:30',82.9,15.75,0,0,1,'','2013-10-31 21:31',2)
+ 
+
+ 
+ -- Table 2   19:00开台  2人消费 正在用餐  19:00点第一轮菜 2个 上菜完毕  19:30点第二轮菜2个 打印至厨房 等待上菜 服务员ID=2  At Restaurant 消费税19% 
+INSERT INTO [dbo].[TableMeal] values (2,2,4,'2013-10-31 19:00','',2)
+ --19:00第一轮点菜 19:20上菜完毕 服务员2
+INSERT INTO [dbo].[Order] values (2,1,3,2,'2013-10-31 19:00',2,'2013-10-31 19:20',2,1)
+INSERT INTO [dbo].[TableMealOrder] values (2,2,2)
+INSERT INTO [dbo].[OrderItem] values (5,2,1,1)
+INSERT INTO [dbo].[OrderItem] values (6,2,2,1)
+--19：30第二轮点菜 服务员2 已打印至厨房 正在等待
+INSERT INTO [dbo].[Order] values (3,1,2,2,'2013-10-31 19:30',2,'',2,1)
+INSERT INTO [dbo].[TableMealOrder] values (3,2,3)
+INSERT INTO [dbo].[OrderItem] values (7,3,10,1)
+INSERT INTO [dbo].[OrderItem] values (8,311,1)
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
